6dbd0906a935659926550f5f4646cb288a4b5069
taylorgehrts <taylorgehrts@gmail.com>
2023-10-02 18:16:58 +0200

    Update MongoDB connection and other changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+let mongoURI;
+
+if (process.env.NODE_ENV === 'production') {
+  // Heroku-provided MongoDB URI
+  mongoURI = process.env.MONGODB_URI;
+} else {
+  // Local MongoDB URI
+  mongoURI = 'mongodb://127.0.0.1:27017/googlebooks';
+}
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose.connection;
